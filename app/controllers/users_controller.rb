a9905c74aab0339255505c410d0d8f59ea6acfa7
github username <github email address>
2019-02-12 11:34:33 -0500

    Login Page and Messages Rendered

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,17 @@ class UsersController < ApplicationController
     convos1 = Conversation.all.select{|c| c.user_id1 == @user.id}
     convos2 = Conversation.all.select{|c| c.user_id2 == @user.id}
     allConvos = convos1 + convos2
-    render json: {user: @user, conversations: allConvos}
+
+    @users = User.all
+
+    arrayOfConvoObjs = allConvos.map do |convoObj|
+      { convo_id: convoObj.id,
+        user_id1: User.find(convoObj.user_id1),
+        user_id2: User.find(convoObj.user_id2)
+      }
+    end
+
+    render json: {user: @user, conversations: arrayOfConvoObjs}
   end
 
   def findUsername
--- app/serializers/conversation_serializer.rb
+++ app/serializers/conversation_serializer.rb
@@ -1,4 +1,4 @@
 class ConversationSerializer < ActiveModel::Serializer
-  attributes :id, :user_id1, :user_id2, :messages, :users
+  attributes :id, :user_id1, :user_id2, :messages, :users,:message
   has_many :messages, :users
 end
